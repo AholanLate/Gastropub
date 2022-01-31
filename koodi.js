bc74f4f4763314cc162d513bad99d36c2b483b32
vehk11s <lari.vehkalampi@student.hamk.fi>
2022-01-31 21:31:03 +0200

    Contentin linkkejä muokattu
    Vaihdettu contentin linkit buttoneista linkeiksi.

--- koodi.js
+++ koodi.js
@@ -1,28 +1,28 @@
       
       //Kun koko ikkunan kaikki elementit ovat latautuneet
-      window.onload = function() {
-        //haetaan nappi id:n perusteella ja tallennetaan se muuttujaan
-        const btn = document.getElementById("hamburger-button");
+  window.onload = function() {
+    //haetaan nappi id:n perusteella ja tallennetaan se muuttujaan
+    const btn = document.getElementById("hamburger-button");
 
-        //Jos lnappia painetaan, ajetaan seuraava koodi
-        btn.onclick = function() {
+    //Jos lnappia painetaan, ajetaan seuraava koodi
+    btn.onclick = function() {
 
-        //etitään laajennettava div id:n perusteella ja tallennetaan se muuttujaan
-        let vElement = document.getElementById("collapsible-div");
-        //Haetaan divin tyylit
-        let vStyle = window.getComputedStyle(vElement);
-        //Haetaan tyyleistä visibilityn arvo ja tallennetaan se muuttujaan
-        let vVisibility = vStyle.getPropertyValue('visibility');
+    //etitään laajennettava div id:n perusteella ja tallennetaan se muuttujaan
+    let vElement = document.getElementById("collapsible-div");
+    //Haetaan divin tyylit
+    let vStyle = window.getComputedStyle(vElement);
+    //Haetaan tyyleistä visibilityn arvo ja tallennetaan se muuttujaan
+    let vVisibility = vStyle.getPropertyValue('visibility');
 
-        //jos arvo on hidden vaihdetaan se visible, muuten asetetaan arvoksi hidden
-        if (vVisibility === "hidden"){
-          vElement.style.visibility = "visible";
-        }
-        else{
-          vElement.style.visibility = "hidden";
-        }
+    //jos arvo on hidden vaihdetaan se visible, muuten asetetaan arvoksi hidden
+    if (vVisibility === "hidden"){
+        vElement.style.visibility = "visible";
+    }
+    else{
+      vElement.style.visibility = "hidden";
+    }
 
-        //koska ei vaihdeta sivua
-        return false;
+      //koska ei vaihdeta sivua
+      return false;
     }
   }
